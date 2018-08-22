5ee41bf1f0cd7c89a32c76139402956740cb3e01
nzxwang <nickywang100@gmail.com>
2018-08-22 17:24:34 -0400

    tibble-ized

--- sampling_the_imaginary.R
+++ sampling_the_imaginary.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(rethinking)
 
-2PrP_g_V <- 0.95
+PrP_g_V <- 0.95
 PrP_g_M <- 0.01
 PrV <- 0.001
 #Probability of Vampire given Positive test
@@ -18,7 +18,7 @@ globe_toss <- tibble(p_grid = seq( from=0 , to=1 , length.out=1000 ),
 globe_toss %>% ggplot() + geom_col(aes(p_grid, posterior))
 
 #sample the posterior probability
-samples <- base::sample(globe_toss$p_grid,
+samples <- sample(globe_toss$p_grid,
                    size=1000,
                    replace=TRUE,
                    prob=globe_toss$posterior)
@@ -33,29 +33,38 @@ sum( samples<0.5) /1000
 # 3.2.2 Interval of defined mass
 quantile(samples, 0.8)
 quantile(samples, c(0.1,0.9))
+quantile(samples, c(0.25,0.75))
 HPDI(samples, prob=0.5)
 
-
-p_grid <- seq( from=0 , to=1 , length.out=1000 )
-prior <- rep(1,1000)
-likelihood <- dbinom( 3 , size=3 , prob=p_grid )
-posterior <- likelihood * prior
-posterior <- posterior / sum(posterior)
-samples <- sample( p_grid , size=1e4 , replace=TRUE , prob=posterior )
+globe_toss_skewed <- tibble(
+  p_grid = seq( from=0 , to=1 , length.out=1000 ),
+  prior = rep(1,1000),
+  likelihood = dbinom( 3 , size=3 , prob=p_grid ),
+  posterior = likelihood * prior / sum(likelihood * prior))
+samples_skewed = sample( globe_toss_skewed$p_grid,
+                         size=1e4,
+                         replace=TRUE,
+                         prob=globe_toss_skewed$posterior )
+as_tibble(samples_skewed) %>% ggplot() + geom_histogram(aes(value))
 
 #percentile interval using the package rethinking
-PI( samples , prob=0.5 ) #quantile(samples, c(0.25,0.75))
+PI( samples_skewed , prob=0.5 ) #quantile(samples, c(0.25,0.75))
 #highest posterior density interval
-HPDI( samples , prob=0.5 )
+HPDI( samples, prob=0.5 )
+HPDI( samples_skewed , prob=0.5 )
 
 #maximum a posteriori
-p_grid[ which.max(posterior) ]
-chainmode( samples , adj=0.01 )
+globe_toss$p_grid[ which.max(globe_toss$posterior) ]
+globe_toss_skewed$p_grid[ which.max(globe_toss_skewed$posterior) ]
+chainmode( samples_skewed , adj=0.01 )
 
-mean(samples)
-median(samples)
+mean(samples_skewed)
+median(samples_skewed)
 
+#loss function is just the absolute distance hence we optimize for median.
 guess <- 0.5
-sum( posterior*abs( guess - p_grid ) )
-loss <- sapply( p_grid , function(d) sum( posterior*abs( d - p_grid ) ) )
-p_grid[ which.min(loss) ]
+sum( globe_toss_skewed$posterior*abs( guess - globe_toss_skewed$p_grid))
+loss <- sapply(globe_toss_skewed$p_grid, 
+              function(d) sum(globe_toss_skewed$posterior * 
+                                abs(d - globe_toss_skewed$p_grid) ))
+globe_toss_skewed$p_grid[ which.min(loss) ]
